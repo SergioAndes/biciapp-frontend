fef43f2ee1436c75ea176ecc87fa575d2eb355d7
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2020-05-16 18:40:37 -0500

    cambio envrionmenrt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: false
+  production: true,
+  urlBackDjango: 'http://127.0.0.1:8000',
+  urlBackJava: 'https://bicibff.herokuapp.com/bicibff/'
 };
+
+
+export const API_URL = 'http://127.0.0.1:8000/api/';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   urlBackDjango: 'http://127.0.0.1:8000',
   urlBackJava: 'https://bicibff.herokuapp.com/bicibff/'
 };
